f7b38d2b8fba60d09d21b513771323926715ad15
Boffee <zhenbangli@gmail.com>
2023-05-23 01:42:25 -0500

    doc: add reminder to reserve constant entities

--- packages/contracts/src/constants.sol
+++ packages/contracts/src/constants.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+// TODO: set owner of constant entities
 bytes32 constant NULL = bytes32("NULL");
+
+bytes32 constant PLAYER_TOKEN = bytes32("PLAYER_TOKEN");
+bytes32 constant ITEM_TOKEN = bytes32("ITEM_TOKEN");
+bytes32 constant GOLD_TOKEN = bytes32("GOLD_TOKEN");
 
 bytes32 constant ON_ENTER = bytes32("ON_ENTER");
 bytes32 constant ON_LEAVE = bytes32("ON_LEAVE");
@@ -11,9 +16,5 @@ bytes32 constant ON_TRIGGER = bytes32("ON_TRIGGER");
 bytes32 constant ON_ATTACK = bytes32("ON_ATTACK");
 bytes32 constant ON_HIT = bytes32("ON_HIT");
 bytes32 constant ON_KILL = bytes32("ON_KILL");
-
-bytes32 constant PLAYER_TOKEN = bytes32("PLAYER_TOKEN");
-bytes32 constant ITEM_TOKEN = bytes32("ITEM_TOKEN");
-bytes32 constant GOLD_TOKEN = bytes32("GOLD_TOKEN");
 
 bytes32 constant DUMMY = bytes32("DUMMY");
