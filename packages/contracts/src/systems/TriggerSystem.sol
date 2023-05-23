e294899c88f12cde66a9295febeef0cd748de146
Boffee <zhenbangli@gmail.com>
2023-05-22 21:38:01 -0500

    feat: limit client trigger to 1 tile

--- packages/contracts/src/systems/TriggerSystem.sol
+++ packages/contracts/src/systems/TriggerSystem.sol
@@ -4,9 +4,11 @@ pragma solidity >=0.8.0;
 import {IWorld} from "../codegen/world/IWorld.sol";
 import {AuthedSystem} from "../extensions/AuthedSystem.sol";
 import {ON_TRIGGER} from "../constants.sol";
+import {PositionLib} from "../libraries/PositionLib.sol";
 
 contract TriggerSystem is AuthedSystem {
   function trigger(bytes32 source, bytes32 target) public onlyApproved(source) {
+    require(PositionLib.withinDistance(source, target, 1), "too far away");
     IWorld(_world())._trigger(source, target);
   }
 }
